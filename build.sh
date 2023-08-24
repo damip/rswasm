e2d6cd13b92a8192128a4dae4a9028bb84700c4b
Damir Vodenicarevic <damipator@gmail.com>
2023-08-24 20:14:56 +0200

    updates

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 INPUT_PATH="target/wasm32-unknown-unknown/release/rust_wasm_guest.wasm"
 OUTPUT_PATH="rust_wasm_guest.wasm"
 
-cargo build --target wasm32-unknown-unknown --release && \
+RUSTFLAGS='-C debuginfo=0' cargo build --target wasm32-unknown-unknown --release && \
     wasm-opt -Oz -o $OUTPUT_PATH $INPUT_PATH && \
     wasm-strip $OUTPUT_PATH
